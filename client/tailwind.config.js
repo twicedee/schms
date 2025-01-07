fa80f1b355c2dea22a2b2130069784da162c4c4e
twicedee <okumudesy@gmail.com>
2025-01-07 19:24:58 +0300

    deplyment 1

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,18 @@
+import { Flowbite } from "flowbite-react";
+
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    Flowbite.content(),
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin'),
+    Flowbite.plugin(),
   ],
 };
