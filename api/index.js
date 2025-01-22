66d929eaf99b0e91088e3420663b8e64cb9f3097
twicedee <okumudesy@gmail.com>
2025-01-22 20:42:30 +0300

    Deployment 1

--- api/index.js
+++ api/index.js
@@ -33,8 +33,8 @@ app.use('/api/fee', feeRoutes)
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('/api', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/dist/index.html'));
 });
 
 
