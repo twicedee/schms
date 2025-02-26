4ecf339d8724f2a3c3c06d6a8dd2b86e655dbd8a
twicedee <okumudesy@gmail.com>
2025-02-27 00:37:50 +0300

    home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Carousel } from "flowbite-react";
 
 export default function Home() {
   return (
-    <></>
-  )
+    <div className="m-5 flex-col gap-4">
+      <div className="flex flex-auto flex-row mx-auto h-32 ">
+        <div>
+          <img className="h-28 w-28" src="MCCPLOGO.png" />
+        </div>
+        <div className="flex-col justify-center m-auto font-serif">
+          <p className="text-4xl font-extrabold">Mashimoni Moyo</p>
+          <p className="text-2xl  italic">Learn Today Lead Tomorrow</p>
+        </div>
+      </div>
+
+      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <Carousel leftControl=" " rightControl=" ">
+          <img src="IMG-20250205-WA0001.jpg" alt="..." />
+          <img src="IMG-20250205-WA0000.jpg" alt="..." />
+          <img src="IMG-20250122-WA0002.jpg" alt="..." />
+          <img src="IMG-20250122-WA0001.jpg" alt="..." />
+        </Carousel>
+      </div>
+      <div className="flex h-56  m-3 p-2">
+        <div className="flex-col justify-center m-auto font-serif">
+          <p className="text-6xl justify-center font-bold">Mission:</p>
+          <p className="text-4xl pt-4 font-thin">Empowering Tomorrow Leaders</p>
+        </div>
+      </div>
+    </div>
+  );
 }
